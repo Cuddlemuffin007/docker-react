14752d05fd1792b23f30cd37c114ee0b29b374b5
Cuddlemuffin007 <brennonkrogers@gmail.com>
2019-03-09 12:54:52 +0900

    initial commit setting up TravisCI

---
+++ build/precache-manifest.0f4ed802735c0ff15579f88e3fb393e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "abc49eff8c4e3d6d9d82",
+    "url": "/static/css/main.8bbe38d2.chunk.css"
+  },
+  {
+    "revision": "abc49eff8c4e3d6d9d82",
+    "url": "/static/js/main.abc49eff.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e15aaeebfa491786421badb1bf835019",
+    "url": "/index.html"
+  }
+];
